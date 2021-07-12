77a561fceb81daa1b987a779c4ec7a92b7ce2e26
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-12 10:46:10 +0530

    Addded: Employee Attedance code added

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,18 @@
-public class EmpWageBuilder
-{
-        public static void main (String args[])
-        {
-                System.out.println("welocome to the EmpWageBuilder Program");
-        }
+public class EmpWageBuilderUC1 {
 
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random()*10)/2;
+		System.out.println(empCheck);
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("presemt");
+		else
+			System.out.println("absent");
+	}
 
 }
+
 
 
 
