aac073bb304a03671071b7e7fc602da57b69aeaf
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-12 10:35:19 +0530

    welcome

---
+++ EmpWage.java
@@ -0,0 +1,15 @@
+public class EmpWageBuilder
+{
+        public static void main (String args[])
+        {
+                System.out.println("welocome to the EmpWageBuilder Program");
+        }
+
+
+
+}
+
+
+
+
+
